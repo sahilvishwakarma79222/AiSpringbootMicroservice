06b40fcb0404c30b5404ff4b4d270123620afa0e
vsahil79222 <vsahil79222@gmail.com>
2026-09-19 17:37:50 +0530

    added rabbit mq

--- QuestionGenerator/questionsgenerator/src/main/java/com/questionsgenerator/functions/QuizService.java
+++ QuestionGenerator/questionsgenerator/src/main/java/com/questionsgenerator/functions/QuizService.java
@@ -4,16 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 @Configuration
 public class QuizService {
 
 
+//    @Bean
+//    public Consumer<QuizDto> getQuizBinding(){
+//        return quizDto -> {
+//            System.out.println("Quiz Created Event Received!!!!");
+//            System.out.println(quizDto.getTitle());
+//        };
+//    }
+
     @Bean
-    public Consumer<QuizDto> getQuizBinding(){
-        return quizDto -> {
-            System.out.println("Quiz Created Event Received!!!!");
-            System.out.println(quizDto.getTitle());
+    public Function<QuizDto,String> getQuizBinding(){
+
+        return information->{
+            System.out.println(information.getTitle());
+            System.out.println(information.getCategoryId());
+            return "Quiz Created succesfully!!!";
         };
     }
 }
